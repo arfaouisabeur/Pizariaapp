63c68ae6f333d29d28d3dff60eb41ecdefd5a729
Saber Arfaoui <arfaouisaber140@gmail.com>
2023-07-08 18:41:49 +0200

    first v

---
+++ src/compnents/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        
+      </div>
+      <p> &copy; 2021 pedrotechpizza.com</p>
+    </div>
+  );
+}
+
+export default Footer;
